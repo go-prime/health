366949b181e3e88197f10bf5a034c7c6ff2932b5
tinomhishi <tinotendamhishi@protonmail.com>
2024-07-23 11:29:15 +0200

    Hiding fields

--- healthcare/healthcare/doctype/patient_appointment/patient_appointment.js
+++ healthcare/healthcare/doctype/patient_appointment/patient_appointment.js
@@ -122,6 +122,8 @@ frappe.ui.form.on('Patient Appointment', {
 		frm.set_df_property('section_break_16', 'hidden', 1)
 		frm.set_df_property('add_video_conferencing', 'hidden', 1)
 		frm.set_df_property('practitioner', 'hidden', 1)
+		frm.set_df_property('practitioner_name', 'hidden', 1)
+		frm.set_df_property('department', 'hidden', 1)
 	},
 
 	make_invoice_button: function (frm) {
