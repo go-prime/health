8b7bf3f69391c32f1474b3f4c8f0f858bff68585
georgejtee <georgejanasi@gmail.com>
2024-07-24 13:37:04 +0200

    Made changes to popup and clinical procedure doctype

--- healthcare/public/js/sales_invoice.js
+++ healthcare/public/js/sales_invoice.js
@@ -74,12 +74,50 @@ var get_consumables_to_invoice = function(frm){
 			{ fieldtype: 'HTML', fieldname: 'results_area' }
 		]
 	});
-
+	var $wrapper;
+	var $results;
+	var $placeholder;
 	dialog.set_values({
 		'patient': frm.doc.patient
 	});
-	
-
+	dialog.fields_dict["patient"].df.onchange = () => {
+		var patient = dialog.fields_dict.patient.input.value;
+		if(patient && patient!=selected_patient){
+			selected_patient = patient;
+			var columns = (["item_code", "item_name","reference_name", "reference_type", "qty" ]);
+			frappe.call({
+				method: "healthcare.healthcare.utils.get_consumables_to_invoice",
+				args: {patient: patient, company: frm.doc.company},
+			}).then(data => {
+				if(data.message){
+					$results.append(make_list_row(columns, true));
+					for(let i=0; i<data.message.length; i++){
+						$results.append(make_list_row(columns, true, data.message[i]));
+					}
+			};
+		})
+		}
+		else if(!patient){
+			selected_patient = '';
+			$results.empty();
+			$results.append($placeholder);
+		}
+	}
+	$wrapper = dialog.fields_dict.results_area.$wrapper.append(`<div class="results"
+		style="border: 1px solid #d1d8dd; border-radius: 3px; height: 300px; overflow: auto;"></div>`);
+	$results = $wrapper.find('.results');
+	$placeholder = $(`<div class="multiselect-empty-state">
+				<span class="text-center" style="margin-top: -40px;">
+					<i class="fa fa-2x fa-heartbeat text-extra-muted"></i>
+					<p class="text-extra-muted">No Consumables found</p>
+				</span>
+			</div>`);
+	$results.on('click', '.list-item--head :checkbox', (e) => {
+		$results.find('.list-item-container .list-row-check')
+			.prop("checked", ($(e.target).is(':checked')));
+	});
+	set_primary_action(frm, dialog, $results, true);
+	dialog.show();
 }
 
 
@@ -239,6 +277,10 @@ var get_checked_values= function($results) {
 			else{
 				checked_values['description'] = false;
 			}
+			if(checked_values['rate'] == null ||  checked_values['rate'] == "null" ){
+				checked_values['rate'] = 0
+			}
+
 			return checked_values;
 		}
 	}).get();
